2ba4d930a49889bb0b41121c53e2c0383a3842ff
TimdeLaater <74412669+TimdeLaater@users.noreply.github.com>
2023-02-02 10:06:28 +0100

    Made the old code functional in VS

---
+++ BiscoopApp/Domain/MovieTicket.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiscoopApp.Domain
+{
+    public class MovieTicket
+    {
+        private int RowNr;
+        private int SeatNr;
+        private bool IsPremium;
+        private bool isStudentOrder;
+        public MovieScreening MovieScreening { get; }
+
+        public MovieTicket(MovieScreening movieScreening, int rowNr, int seatNr, bool isPremiumReservation)
+        {
+            MovieScreening = movieScreening;
+            RowNr = rowNr;
+            SeatNr = seatNr;
+            IsPremium = isPremiumReservation;
+        }
+        public bool IsPremiumTicket()
+        {
+            return IsPremium;
+        }
+        public double GetPrice()
+        {
+            return MovieScreening.GetPricePerSeat();
+        }
+
+        public override string? ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
--- BiscoopApp/Program.cs
+++ BiscoopApp/Program.cs
@@ -1,2 +1,54 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using BiscoopApp.Domain;
+
 Console.WriteLine("Hello, World!");
+Movie movie1 = new Movie("Revenge of the Sith");
+MovieScreening movieScreening1 = new MovieScreening(movie1, DateTime.Now, 10);
+movie1.AddScreening(movieScreening1); Order order = new Order(8, false);
+order.AddSeatReservation(new MovieTicket(movieScreening1, 1, 1, true));
+Console.WriteLine(order.CalculatePrice());
+order.Export(TicketExportFormat.JSON);
+
+// DirectoryInfo di = new DirectoryInfo("./Deel1/");
+
+// string pathString = di.FullName + "Orders";
+
+// string fileName = "test" + ".txt";
+// pathString = System.IO.Path.Combine(pathString, fileName);
+
+// // Verify the path that you have constructed.
+// Console.WriteLine("Path to my file: {0}\n", pathString);
+// if (!System.IO.File.Exists(pathString))
+// {
+//     using (System.IO.FileStream fs = System.IO.File.Create(pathString))
+//     {
+//         for (byte i = 0; i < 100; i++)
+//         {
+//             fs.WriteByte(i);
+//         }
+//     }
+// }
+// else
+// {
+//     Console.WriteLine("File \"{0}\" already exists.", fileName);
+//     return;
+// }
+
+// // Read and display the data from your file.
+// try
+// {
+//     byte[] readBuffer = System.IO.File.ReadAllBytes(pathString);
+//     foreach (byte b in readBuffer)
+//     {
+//         Console.Write(b + " ");
+//     }
+//     Console.WriteLine();
+// }
+// catch (System.IO.IOException e)
+// {
+//     Console.WriteLine(e.Message);
+// }
+
+// // Keep the console window open in debug mode.
+// System.Console.WriteLine("Press any key to exit.");
+// System.Console.ReadKey();
