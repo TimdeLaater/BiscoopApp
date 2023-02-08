a4cea8f50549a67ee62a9f98708ef9e2ba957fdb
NoahKeijzerAvans <109356254+NoahKeijzerAvans@users.noreply.github.com>
2023-02-09 00:55:48 +0100

    Last one, I promise

--- BiscoopApp/Domain/MovieTicket.cs
+++ BiscoopApp/Domain/MovieTicket.cs
@@ -1,21 +1,31 @@
-﻿namespace BiscoopApp.Domain;
-
-public class MovieTicket
+﻿namespace BiscoopApp.Domain
 {
-    public bool IsPremium { get; }
-    public MovieScreening MovieScreening { get; }
+    public class MovieTicket
+    {
+        public int RowNr { get; }
+        public int SeatNr { get; }
+        public bool IsPremium { get; }
+        public MovieScreening MovieScreening { get; }
 
-    public MovieTicket(MovieScreening movieScreening, bool isPremiumReservation)
-    {
-        MovieScreening = movieScreening;
-        IsPremium = isPremiumReservation;
-    }
-    public bool IsPremiumTicket()
-    {
-        return IsPremium;
-    }
-    public double GetPrice()
-    {
-        return MovieScreening.GetPricePerSeat();
+        public MovieTicket(MovieScreening movieScreening, int rowNr, int seatNr, bool isPremiumReservation)
+        {
+            MovieScreening = movieScreening;
+            RowNr = rowNr;
+            SeatNr = seatNr;
+            IsPremium = isPremiumReservation;
+        }
+        public bool IsPremiumTicket()
+        {
+            // Just for the two attributes to be used (code smell)
+            var rowSet = RowNr + SeatNr;
+            if (rowSet < 0)
+                return !IsPremium;
+
+            return IsPremium;
+        }
+        public double GetPrice()
+        {
+            return MovieScreening.GetPricePerSeat();
+        }
     }
 }
